6bcd83029dd198ca1e51fe5de0d293614c5d0e9f
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-04-01 10:51:30 +0200

    add end of the photos information

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Results.scss';
 
 export default function Results({ photos }) {
-    // useEffect(() => {
-    //     console.log(photos);
-    // }, []);
+    let [reachEnd, setReachEnd] = useState(false);
+    useEffect(() => {
+        if (photos.length === 30) {
+            setReachEnd(true);
+        }
+    }, [photos]);
 
     if (photos.length === 0) {
         return <p>No results.</p>;
     } else {
         return (
-            <div className="results-container">
-                {photos.map((element) => (
-                    <img
-                        src={
-                            'http://source.unsplash.com/' +
-                            element.url.replace(
+            <>
+                <div className="results-container">
+                    {photos.map((element) => (
+                        <img
+                            src={
+                                'http://source.unsplash.com/' +
+                                element.url.replace(
+                                    'https://unsplash.com/photos/',
+                                    ''
+                                )
+                            }
+                            alt={`author: ${element.author}`}
+                            key={element.url.replace(
                                 'https://unsplash.com/photos/',
                                 ''
-                            )
-                        }
-                        alt={`author: ${element.author}`}
-                        key={element.url.replace(
-                            'https://unsplash.com/photos/',
-                            ''
-                        )}
-                    />
-                ))}
-            </div>
+                            )}
+                        />
+                    ))}
+                </div>
+                {reachEnd && <p>These are all the photos</p>}
+            </>
         );
     }
 }
